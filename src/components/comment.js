cf0df9593f46488304c969f3efed79899830ef0c
Marcus Francis <marcusmagnusson@outlook.com>
2019-02-08 03:54:34 +0100

    Update comment.js

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-export default () => {
-  return <div> comment </div>;
-};
+class Comment extends React.Component {
+
+  state = { comment: '' };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    //TODO: Save the comment
+
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4> Comments </h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button> Add a comment </button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default Comment;
