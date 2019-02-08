9fa1c3b21d4acc50496bbd89ce9a093ac7cf68cc
Marcus Francis <marcusmagnusson@outlook.com>
2019-02-08 03:31:32 +0100

    Update App.test.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
+import Comment from './comment';
+import CommentList from './commentList';
+
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('renders comment component', () => {
+  expect(wrapped.find(Comment).length).toEqual(1);
+});
+
+it('renders commentList component', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
