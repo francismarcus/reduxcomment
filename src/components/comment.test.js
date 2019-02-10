f8192b149afabe9e6f23c62c9f006c637a35babe
Marcus Francis <marcusmagnusson@outlook.com>
2019-02-11 00:25:35 +0100

    +comment.test.js

---
+++ src/components/comment.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Comment from './comment';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<Comment />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('Shows a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
+});
+
+it('has a textarea users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment'}
+  });
+
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('when the form is submitted, text area should get emptied', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  }),
+
+  wrapped.update();
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
