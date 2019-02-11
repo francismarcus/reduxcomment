8de00c1c1a463c74dc2dfd0dd67d06eed9ab92e2
Marcus Francis <marcusmagnusson@outlook.com>
2019-02-11 03:12:10 +0100

    +reducers/comments.test.js

---
+++ src/reducers/comments.test.js
@@ -0,0 +1,12 @@
+import commentsReducer from './comments';
+import { SAVE_COMMENT } from '../actions/types';
+
+it('handles actions of type SAVE_COMMENT', () => {
+    const action = {
+        type: SAVE_COMMENT,
+        payload: 'New comment'
+    };
+
+    const newState = commentsReducer([], action)
+    expect(newState).toEqual(['New comment']);
+});
